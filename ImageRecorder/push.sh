bb436a1b2a56470cf8295f062247819a4faa0e77
samuel.modeste <samuel.m.modeste@outlook.com>
2024-02-18 18:43:25 +0400

    Set hardcoded images folder

--- ImageRecorder/push.sh
+++ ImageRecorder/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-dotnet /app/ImageRecorder.dll --rabbitMqHostName=$RABBITMQ_HOSTNAME --queueName=$RABBITMQ_QUEUE_NAME --imagesDirPath=$IMAGES_DIR_PATH --cameraName=$CAMERA_NAME --routingKey=$ROUTING_KEY
+dotnet /app/ImageRecorder.dll --rabbitMqHostName=$RABBITMQ_HOSTNAME --queueName=$RABBITMQ_QUEUE_NAME --imagesDirPath=/images --cameraName=$CAMERA_NAME --routingKey=$ROUTING_KEY
 echo "ImageRecorder crashed Respawning after 5s.."
 sleep 5
 done
