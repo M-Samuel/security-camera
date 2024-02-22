b5ba967e6890fe50f3380571e531b37a37444202
samuel.modeste <samuel.m.modeste@outlook.com>
2024-02-22 16:44:27 +0400

    Added DOTNET ONNX project detection

---
+++ ObjectDetector/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace ObjectDetector.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
