806d0337be3714070419be893f0827fc9338e267
M-Samuel <samuel.m.modeste@gmail.com>
2024-03-07 06:42:53 +0000

    Implemented RabbitMq

--- SecurityCamera.Domain/ImageRecorderDomain/IImageRecorderService.cs
+++ SecurityCamera.Domain/ImageRecorderDomain/IImageRecorderService.cs
@@ -7,7 +7,7 @@ namespace SecurityCamera.Domain.ImageRecorderDomain;
 public interface IImageRecorderService
 {
     Task<Result<ImageRecordedEvent[]>> ScanDirectory(StartDirectoryScanEvent startDirectoryScanEvent, string cameraName, CancellationToken cancellationToken);
-    Task<Result<DetectionEvent?>> LauchDectectionAlogirthm(ImageRecordedEvent imageRecordedEvent, CancellationToken cancellationToken);
+    Task<Result<DetectionEvent?>> LaunchDectectionAlogirthm(ImageRecordedEvent imageRecordedEvent, CancellationToken cancellationToken);
     Task<Result<ImageDetection>> SaveDetectionToDB(DetectionEvent detectionEvent, CancellationToken cancellationToken);
     Task<Result<QueueMessage>> PushImageToQueue(DetectionEvent detectionEvent, CancellationToken cancellationToken);
 }
