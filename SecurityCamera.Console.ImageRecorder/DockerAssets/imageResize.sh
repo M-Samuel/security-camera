9a011606a7cfa6c3e668955748e7b0ff375c77e3
samuel.modeste <samuel.m.modeste@outlook.com>
2024-03-08 18:00:21 +0400

    Added Shell Scripts

---
+++ SecurityCamera.Console.ImageRecorder/DockerAssets/imageResize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+original_dir="/images/original"
+resized_dir="/images/resized"
+
+while true
+do
+
+    for file in "$original_dir"/*.png; do
+        if [[ -f "$file" ]]; then
+            resized_file="$resized_dir/$(basename "$file" .png).jpg"
+            convert "$file" -resize 1024x768 "$resized_file"
+            echo "File $file has been resized and saved as $resized_file"
+        fi
+        sleep 1;
+    done
+
+sleep 5
+done
