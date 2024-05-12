f3d367e038e8e244f9f952a698ef213af8c329c4
samuel.modeste <samuel.m.modeste@outlook.com>
2024-05-12 15:09:58 +0400

    Removed shell parameters replaced by env vars

--- src/SecurityCamera.Console.ImageObjectDetection/DockerAssets/Run.sh
+++ src/SecurityCamera.Console.ImageObjectDetection/DockerAssets/Run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 while true
 do
-    /app/SecurityCamera.Console.ImageObjectDetection \
-    --AzureServiceBusConnectionString="$EnvAzureServiceBusConnectionString" \
-    --AzureStorageConnectionString="$EnvAzureStorageConnectionString" \
-    --RemoteStorageContainer="$EnvRemoteStorageContainer" \
-    --RemoteStorageFileDirectory="$EnvRemoteStorageFileDirectory" \
-    --ServiceBusQueueImageRecords="$EnvServiceBusQueueImageRecords" \
-    --ServiceBusQueueDetections="$EnvServiceBusQueueDetections" \ 
-    --AzureComputerVisionEndpoint="$EnvAzureComputerVisionEndpoint" \
-    --AzureComputerVisionKey="$EnvAzureComputerVisionKey" 
+    /app/SecurityCamera.Console.ImageObjectDetection
     
     echo "Image Object Detection crashed Respawning after 5s.."
     sleep 5
