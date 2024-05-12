482098560ba0b4308906611e554faa22e2b75464
samuel.modeste <samuel.m.modeste@outlook.com>
2024-05-12 14:46:38 +0400

    Hot fix adding quote to fix path no find error

--- src/SecurityCamera.Console.ImageObjectDetection/DockerAssets/Run.sh
+++ src/SecurityCamera.Console.ImageObjectDetection/DockerAssets/Run.sh
@@ -2,14 +2,14 @@
 while true
 do
     /app/SecurityCamera.Console.ImageObjectDetection \
-    --AzureServiceBusConnectionString=$EnvAzureServiceBusConnectionString \
-    --AzureStorageConnectionString=$EnvAzureStorageConnectionString \
-    --RemoteStorageContainer=$EnvRemoteStorageContainer \
-    --RemoteStorageFileDirectory=$EnvRemoteStorageFileDirectory \
-    --ServiceBusQueueImageRecords=$EnvServiceBusQueueImageRecords \
-    --ServiceBusQueueDetections=$EnvServiceBusQueueDetections \ 
-    --AzureComputerVisionEndpoint=$EnvAzureComputerVisionEndpoint \
-    --AzureComputerVisionKey=$EnvAzureComputerVisionKey 
+    --AzureServiceBusConnectionString="$EnvAzureServiceBusConnectionString" \
+    --AzureStorageConnectionString="$EnvAzureStorageConnectionString" \
+    --RemoteStorageContainer="$EnvRemoteStorageContainer" \
+    --RemoteStorageFileDirectory="$EnvRemoteStorageFileDirectory" \
+    --ServiceBusQueueImageRecords="$EnvServiceBusQueueImageRecords" \
+    --ServiceBusQueueDetections="$EnvServiceBusQueueDetections" \ 
+    --AzureComputerVisionEndpoint="$EnvAzureComputerVisionEndpoint" \
+    --AzureComputerVisionKey="$EnvAzureComputerVisionKey" 
     
     echo "Image Object Detection crashed Respawning after 5s.."
     sleep 5
